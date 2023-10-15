8c217ab9f1a7c16ee00122e7c9be1428b4d49290
ra2dd <radoslawpryw@gmail.com>
2023-10-15 19:12:08 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App(props) {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          {props.title}
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
