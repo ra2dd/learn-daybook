17a04a65654c6c0e2096dcd478f0724d9e67d3d9
ra2dd <radoslawpryw@gmail.com>
2023-10-15 22:30:49 +0200

    add index sample code, add minimal styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 function App(props) {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          {props.title}
-        </p>
-      </header>
-    </div>
+    <main>
+      <form className="daybook-form">
+        <textarea 
+          placeholder="Today I practiced react components ..." 
+          autocomplete="off"
+          rows={6}
+          cols={40}
+        />
+        <button type="submit">Add</button>
+      </form>
+
+      <section>
+        <div className="daybook">
+          <p>I created index page</p>
+          <p>16 October 2023</p>
+        </div>
+      </section>
+    </main>
   );
 }
 
